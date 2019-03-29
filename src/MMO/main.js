e6c6c40efef3868f1a496b9b8b4ea61c740943bd
Northbase <nbae0127@gmail.com>
2019-03-29 13:23:45 -0400

    fixed framework

--- src/MMO/main.js
+++ src/MMO/main.js
@@ -3,14 +3,14 @@ const url = require('url');
 const path = require('path');
 const {app, BrowserWindow} = electron;
 
-let mainWindow
+let window
 
 app.on('ready', function() {
-    mainWindow = new BrowserWindow({width: 1280, height: 720, zoomFactor: 0.8});
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, "index.html"),
+    window = new BrowserWindow({width: 1280, height: 720, zoomFactor: 0.8});
+    window.loadURL(url.format({
+        pathname: path.join(__dirname, "FrontEnd/index.html"), // local HTML import fixed.
         protocol: 'file',
         slashes: true
     }));
-})
+});
 
