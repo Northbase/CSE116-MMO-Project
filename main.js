2d6716f317b57e3cc4b6b5477425a4c1aacc522b
Northbase <nbae0127@gmail.com>
2019-04-01 12:19:01 -0400

    try launching app

---
+++ main.js
@@ -0,0 +1,22 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+const {app, BrowserWindow} = electron;
+
+let window;
+
+app.on('ready', function() {
+    window = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        zoom: 0.8,
+        frame: false,
+        setFullScreen: true
+    });
+    window.loadURL(url.format({
+        pathname: path.join(__dirname, "src/MMO/FrontEnd/index.html"),
+        protocol: 'file',
+        slashes: true
+    }));
+});
+
