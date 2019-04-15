53946d4ca42b06a55541cee44d9bf21897ba41d7
Northbase <nbae0127@gmail.com>
2019-04-15 13:28:13 -0400

    Flask syntax

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 <b>To Start the App:</b>
 type npm start in command.
 
+<b>For Flask Server </b>
+HTML files should be in "templates" directory
+CSS files should be in "static/styles" directory
+
 <b>Controls:</b>
 W A S D: Camera Movement 
 Q: Camera Zoom In
--- test/spec.js
+++ test/spec.js
@@ -3,11 +3,7 @@ const assert = require('assert')
 const electronPath = require('electron') // Require Electron from the binaries included in node_modules.
 const path = require('path')
 
-describe('Application launch', function () {
-    this.timeout(10000)
-
-    beforeEach(function () {
-        this.app = new Application({
+this.app = new Application({
             // Your electron path can be any binary
             // i.e for OSX an example path could be '/Applications/MyApp.app/Contents/MacOS/MyApp'
             // But for the sake of the example we fetch it from our node_modules.
@@ -27,21 +23,69 @@ describe('Application launch', function () {
             // The following line tells spectron to look and use the main.js file
             // and the package.json located 1 level above.
             args: [path.join(__dirname, '..')]
-        })
-        return this.app.start()
-    })
+        });
 
-    afterEach(function () {
-        if (this.app && this.app.isRunning()) {
-            return this.app.stop()
-        }
-    })
+app.start().then(function () {
+    // Check if the window is visible
+    return app.browserWindow.isVisible()
+}).then(function (isVisible) {
+    // Verify the window is visible
+    assert.equal(isVisible, true)
+}).then(function () {
+    // Get the window's title
+    return app.client.getTitle()
+}).then(function (title) {
+    // Verify the window's title
+    assert.equal(title, 'My App')
+}).then(function () {
+    // Stop the application
+    return app.stop()
+}).catch(function (error) {
+    // Log any failures
+    console.error('Test failed', error.message)
+});
 
-    it('shows an initial window', function () {
-        return this.app.client.getWindowCount().then(function (count) {
-            assert.equal(count, 1)
-            // Please note that getWindowCount() will return 2 if `dev tools` are opened.
-            // assert.equal(count, 2)
-        })
-    })
-})
+
+
+// describe('Application launch', function () {
+//     this.timeout(10000);
+//
+//     beforeEach(function () {
+//         this.app = new Application({
+//             // Your electron path can be any binary
+//             // i.e for OSX an example path could be '/Applications/MyApp.app/Contents/MacOS/MyApp'
+//             // But for the sake of the example we fetch it from our node_modules.
+//             path: electronPath,
+//
+//             // Assuming you have the following directory structure
+//
+//             //  |__ my project
+//             //     |__ ...
+//             //     |__ main.js
+//             //     |__ package.json
+//             //     |__ index.html
+//             //     |__ ...
+//             //     |__ test
+//             //        |__ spec.js  <- You are here! ~ Well you should be.
+//
+//             // The following line tells spectron to look and use the main.js file
+//             // and the package.json located 1 level above.
+//             args: [path.join(__dirname, '..')]
+//         });
+//         return this.app.start()
+//     });
+//
+//     afterEach(function () {
+//         if (this.app && this.app.isRunning()) {
+//             return this.app.stop()
+//         }
+//     });
+//
+//     it('shows an initial window', function () {
+//         return this.app.client.getWindowCount().then(function (count) {
+//             assert.equal(count, 1)
+//             // Please note that getWindowCount() will return 2 if `dev tools` are opened.
+//             // assert.equal(count, 2)
+//         })
+//     })
+// });
